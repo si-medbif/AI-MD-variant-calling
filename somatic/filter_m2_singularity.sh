98532504ea7de9314fd666079da04935877ebd7a
haraldgrove <haraldgrove@gmail.com>
2022-02-24 15:09:50 +0700

    Changed GATK version in FilterMutectCalls

--- somatic/filter_m2_singularity.sh
+++ somatic/filter_m2_singularity.sh
@@ -2,16 +2,13 @@
 
 #SBATCH --job-name=FilterMutectCalls
 #SBATCH --ntasks=1
-#SBATCH --output=filtermutectcalls_%j.log
+#SBATCH --output=job%j_filtermutectcalls.log
 #SBATCH --nodes=1
 #SBATCH --nodelist=omega
 #SBATCH --mem=8gb
 #SBATCH --cpus-per-task=1
 
 # Parabricks software and reference resources
-export PB_PATH=/shared/parabrick/parabricks
-export PATH=$PB_PATH:$PATH
-export PB_HOME=/shared/parabrick/parabricks
 export REF=/shared/dataset/parabricks_sample/Ref
 
 VCFFOLDER=$1
@@ -19,9 +16,9 @@ TUMOR=$2
 
 singularity run \
 	--bind ${REF}:/ref,${VCFFOLDER}:/data \
-	/shared/example_data/singularity/gatk.4.0.12.0.sif \
+	/shared/example_data/singularity/gatk.4.2.5.0.sif \
 	gatk --java-options "-Xmx8g" FilterMutectCalls \
 	-R /ref/Homo_sapiens_assembly38.fasta \
-	--stats /data/${TUMOR}_Mutect2FilteringStats.tsv \
+	--filtering-stats /data/${TUMOR}_Mutect2FilteringStats.tsv \
 	-V /data/${TUMOR}_m2.vcf \
 	-O /data/${TUMOR}_m2_filter.vcf
